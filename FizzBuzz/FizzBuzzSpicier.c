34977750aaefdbee020e6ad178a25fd53fc40abd
JFent10 <jfent10@gmail.com>
2023-10-19 15:14:40 -0400

    Fixed FizzBuzz
    Missing pointer

--- FizzBuzz/FizzBuzzSpicier.c
+++ FizzBuzz/FizzBuzzSpicier.c
@@ -4,26 +4,26 @@
 int prompt(int*, int*, char*, char*);
 void fizzBuzz(int, int, char*, char*, int);
 
-void main()
+int main()
 {
     int op1;
     int op2;
-    int repetitions;
+    int reps;
 
     char* str1 = malloc(80);
     char* str2 = malloc(80);
 
-    repetitions = prompt(&op1, &op2, str1, str2);
-    printf("Repetitions is %d", repetitions);
-    fizzBuzz(op1, op2, str1, str2, repetitions);
+    reps = prompt(&op1, &op2, str1, str2);
+    fizzBuzz(op1, op2, str1, str2, reps);
 
     free(str1);
     free(str2);
+    return(0);
 }
 
 int prompt(int* op1, int* op2, char* str1, char* str2)
 {
-    int repetitions;
+    int reps = 0;
     printf("Please input the first multiple you'd like to replace -> ");
     scanf("%d", op1);
 
@@ -36,19 +36,16 @@ int prompt(int* op1, int* op2, char* str1, char* str2)
     printf("Please input the name for multiples of %d -> ", *op2);
     scanf("%79s", str2);
 
-    /*printf("Please input the number of numbers you'd like to display -> ");
-    scanf("%d", repetitions);
-    */
-    repetitions = op1;
+    printf("Please input how many numbers should be displayed -> ");
+    scanf("%d", &reps);
 
-    printf("check");    
-    return(repetitions);
+    return(reps);
 }
 
 void fizzBuzz(int op1, int op2, char* str1, char* str2, int repetitions)
 {
     int i;
-    for (i = 0; i <= repetitions; i++)
+    for (i = 0; i <= repetitions && repetitions != 0; i++)
     {
         if (!(i % op1))
         {
@@ -64,5 +61,6 @@ void fizzBuzz(int op1, int op2, char* str1, char* str2, int repetitions)
         }
         printf("\n");
     }
+    printf("Tast Complete");
     return;
 }
